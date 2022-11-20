844de5b555c1e221a52e876c8182fc1d5cdb4b0d
gaia <gaia.difederico@mail.polimi.it>
2022-11-20 15:17:01 +0100

    Aggiunta casette

---
+++ Assets/StartGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class StartGame : MonoBehaviour
+{
+ public GameObject InsertedName;
+ public string Username;
+
+ public void Update()
+ {
+  Username = InsertedName.GetComponent<TMP_InputField>().text;
+  
+ }
+
+ public void Start()
+ {
+  PlayerPrefs.SetString("username", Username);
+  //SceneManager.LoadScene("Level1");
+  
+
+ }
+}
