b513d27500459f399356c5cc18485cb576e3bce4
Marco Adimari <marco.adimari@mail.polimi.it>
2022-11-21 18:57:09 +0100

    timer

---
+++ Assets/timerscript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerscript : MonoBehaviour
+{
+  public float count = 10.0f;
+  // public int seconds;
+  public TextMeshProUGUI counttext;
+
+  private bool drop;
+  private float spawnNow;
+
+  void Start()
+  {
+    counttext = GetComponent<TextMeshProUGUI>();
+  }
+
+  void Update()
+  {
+    drop = false;
+
+    if (Input.GetKey(KeyCode.DownArrow))
+    {
+      drop = true;
+    }
+
+    if (count >= 0 && drop == false)
+    {
+      count -= Time.deltaTime;
+      counttext.text = count.ToString();
+    }
+    else if(drop == true && count >= 0)
+    {
+      count = 10;
+    }
+    else if (count <= 0)
+    {
+      count = 10;
+    }
+  }
+  
+}
