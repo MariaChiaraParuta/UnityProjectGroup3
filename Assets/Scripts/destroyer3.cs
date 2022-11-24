1249a2685d3323d49de1139b1f931e05eb26ad61
Marco Adimari <marco.adimari@mail.polimi.it>
2022-11-24 18:43:39 +0100

    Livello 3

---
+++ Assets/Scripts/destroyer3.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class destroyer3 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //LIVELLO2: associare il COLORE
+        if (col.gameObject.name == "House1") //forma: RETTANGOLO
+        {
+            if ((gameObject.name == "Giftrect-blue(Clone)")||
+                (gameObject.name == "Giftrect-red(Clone)")||
+                (gameObject.name == "Giftrect-yellow(Clone)")||
+                (gameObject.name == "Giftrect-green(Clone)"))
+            {
+                TextScript.score += 1;
+            }
+        }
+        
+        if (col.gameObject.name == "House2") //forma: CARTA
+        {
+            if ((gameObject.name == "Gifthouse-blue(Clone)")||
+                (gameObject.name == "Gifthouse-red(Clone)")||
+                (gameObject.name == "Gifthouse-yellow(Clone)")||
+                (gameObject.name == "Gifthouse-green(Clone)"))            {
+                TextScript.score += 1;
+            }
+        }
+        
+        if (col.gameObject.name == "House3")//forma: CUBO
+        {
+            if((gameObject.name == "Giftbox-blue(Clone)")||
+               (gameObject.name == "Giftbox-red(Clone)")||
+               (gameObject.name == "Giftbox-yellow(Clone)")||
+               (gameObject.name == "Giftbox-green(Clone)"))
+            {
+                TextScript.score += 1;
+            }
+        }
+        
+        if (col.gameObject.name == "House4")//forma: CALZA
+        {
+            if ((gameObject.name == "Giftsock-blue(Clone)")||
+                (gameObject.name == "Giftsock-red(Clone)")||
+                (gameObject.name == "Giftsock-yellow(Clone)")||
+                (gameObject.name == "Giftsock-green(Clone)"))
+            {
+                TextScript.score += 1;
+            }
+        }
+        
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/startlevel3.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class startlevel3 : MonoBehaviour
+{
+    public float counttime;
+    public void Start ()
+    {
+        counttime = 0.0f;
+    }
+    
+    public void Update()
+    {
+        // il bottone è attivo solo quando babbo natale finisce di parlare
+        counttime += Time.deltaTime;
+        if(counttime> 15.0f)
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(LoadScene);
+        }
+    }
+    private void LoadScene()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+}
