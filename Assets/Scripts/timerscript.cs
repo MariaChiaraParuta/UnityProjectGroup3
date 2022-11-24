b7070999ecd6f9b2bc7fdca97122740f5a937f17
Marco Adimari <marco.adimari@mail.polimi.it>
2022-11-24 18:27:18 +0100

    Aggiunta countdown livello 1

--- Assets/Scripts/timerscript.cs
+++ Assets/Scripts/timerscript.cs
@@ -20,36 +20,40 @@ public class timerscript : MonoBehaviour
 
   void Update()
   {
-    drop = false;
-    isspawn = false;
-
-    if (GameObject.Find("Giftcard(Clone)") || GameObject.Find("Giftbox(Clone)")
-        || GameObject.Find("Giftrect(Clone)") || GameObject.Find("Giftsock(Clone)"))
+    if (StartTimer.flag_start == 1)
     {
-      isspawn = true;
-    }
-    
-    if (Input.GetKey(KeyCode.DownArrow))
-    {
-      drop = true;
-    }
-
-    if (count >= 0 && drop == false && isspawn == true)
-    {
-      count -= Time.deltaTime;
-      counttext.text = "" + count.ToString("0");
-    }
-    else if(drop == true && count >= 0)
-    {
-      count = 5.2f;
-      counttext.text = "" + count.ToString("0");
+      drop = false;
       isspawn = false;
-    }
-    else if (count <= 0)
-    {
-      count = 5;
-      counttext.text = "" + count.ToString("0");
-      isspawn = false;
+
+      if (GameObject.Find("Giftcard(Clone)") || GameObject.Find("Giftbox(Clone)")
+                                             || GameObject.Find("Giftrect(Clone)") ||
+                                             GameObject.Find("Giftsock(Clone)"))
+      {
+        isspawn = true;
+      }
+
+      if (Input.GetKey(KeyCode.DownArrow))
+      {
+        drop = true;
+      }
+
+      if (count >= 0 && drop == false && isspawn == true)
+      {
+        count -= Time.deltaTime;
+        counttext.text = "" + count.ToString("0");
+      }
+      else if (drop == true && count >= 0)
+      {
+        count = 5.2f;
+        counttext.text = "" + count.ToString("0");
+        isspawn = false;
+      }
+      else if (count <= 0)
+      {
+        count = 5;
+        counttext.text = "" + count.ToString("0");
+        isspawn = false;
+      }
     }
   }
   
