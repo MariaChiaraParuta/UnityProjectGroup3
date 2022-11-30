13c41ed0f451500ce04ebda7c1b9ccc44c069282
Maria Chiara Paruta <mariachiara.paruta@mail.polimi.it>
2022-11-30 13:49:09 +0100

    Altre animazioni pt 2

--- Assets/Scripts/SleighMovement1.cs
+++ Assets/Scripts/SleighMovement1.cs
@@ -29,20 +29,23 @@ public class SleighMovement1 : MonoBehaviour
         {
             nextPosIndex++;
             
-        
 
-         if (nextPosIndex == Positions.Length)
+
+            if (nextPosIndex == Positions.Length)
          {
             nextPosIndex = 0;
+            
          }
 
          nextPos = Positions[nextPosIndex];
+         transform.Rotate(0f, 0f, 0f);
         }
 
         else
         {
             transform.position = Vector2.MoveTowards(transform.position, nextPos.position, objectSpeed * Time.deltaTime);
         }
+        
     }
 }
 
