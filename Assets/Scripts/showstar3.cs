d5ed5c969ad0e0090b53557bf29eb3de4ce3ec35
Marco Adimari <marco.adimari@mail.polimi.it>
2022-12-09 14:37:55 +0100

    condizione tempo per terza stella diversa in base alla difficoltà

--- Assets/Scripts/showstar3.cs
+++ Assets/Scripts/showstar3.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class showstar3 : MonoBehaviour
 {
+    private int timeok;
     // Start is called before the first frame update
     void Start()
         {
             gameObject.SetActive(false);
-            if (TextScript.score >= 8 && timerscript.secNeeded < 25)
+            if (SliderValue.level == 1)
+            {
+                timeok = 48;
+            }
+            else if (SliderValue.level == 2)
+            {
+                timeok = 36;
+            }
+            else if (SliderValue.level == 3)
+            {
+                timeok = 24;
+            }
+            
+            if (TextScript.score >= 8 && timerscript.secNeeded < timeok)
             {
                 gameObject.SetActive(true);
             }
